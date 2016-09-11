9f9a324436e88158893c7aadb506914037754e44
sgiehl <stefan@piwik.org>
2016-09-11 02:14:59 +0200

    update test definitions

--- tests/UI/ReferrersManager_spec.js
+++ tests/UI/ReferrersManager_spec.js
@@ -13,19 +13,20 @@ describe("ReferrersManager", function () {
     it('should load search engines list', function (done) {
         expect.screenshot('searchengines_list').to.be.captureSelector('#content', function (page) {
             page.load("?module=ReferrersManager&action=index&idSite=1&period=day&date=yesterday");
+            page.wait(5000);
         }, done);
     });
 
     it('should identify search engine correct', function (done) {
         expect.screenshot('searchengines_identify').to.be.captureSelector('#content', function (page) {
-            page.sendKeys("#urlToCheck", 'http://www.google.com/xyz/?q=piwik');
-            page.click("#checkurl", 1000);
+            page.sendKeys('[ng-model="urlToCheck"]', 'http://www.google.com/xyz/?q=piwik');
+            page.click('[onconfirm="checkResult()"] input', 1000);
         }, done);
     });
 
     it('should open add search engine dialog', function (done) {
-        expect.screenshot('searchengines_add').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click("[role=addEngine]", 500);
+        expect.screenshot('searchengines_add').to.be.captureSelector('#content', function (page) {
+            page.click('[ng-click="showAddSearchEngineForm(true)"]', 500);
         }, done);
     });
 
@@ -34,26 +35,26 @@ describe("ReferrersManager", function () {
             page.sendKeys("#engineName", 'test', 150);
             page.sendKeys("#engineHost", 'randomhost.com', 150);
             page.sendKeys("#engineParameter", 'q,ghj', 150);
-            page.click('[role="submitAddEngine"]', 150);
+            page.click('[onconfirm="addSearchEngine()"] input', 150);
         }, done);
     });
 
     it('should identify new search engine correct', function (done) {
         expect.screenshot('searchengines_identify_new').to.be.captureSelector('#content', function (page) {
-            page.sendKeys("#urlToCheck", 'http://randomhost.com/xyz/?ghj=piwik');
-            page.click("#checkurl", 1000);
+            page.sendKeys('[ng-model="urlToCheck"]', 'http://randomhost.com/xyz/?ghj=piwik');
+            page.click('[onconfirm="checkResult()"] input', 1000);
         }, done);
     });
 
     it('should show remove search engine dialog correct', function (done) {
-        expect.screenshot('searchengines_remove_dialog').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click("[role=removeEngine]", 500);
+        expect.screenshot('searchengines_remove_dialog').to.be.captureSelector('.modal.open', function (page) {
+            page.click('[ng-click="removeEngine(url.url)"]', 500);
         }, done);
     });
 
     it('should remove new search engine correct', function (done) {
         expect.screenshot('searchengines_list').to.be.captureSelector('#content', function (page) {
-            page.click(".ui-dialog button>span:contains(Yes)", 500);
+            page.click(".modal.open a>span:contains(Yes)", 500);
         }, done);
     });
 
@@ -65,27 +66,27 @@ describe("ReferrersManager", function () {
 
     it('should identify social network correct', function (done) {
         expect.screenshot('social_identify').to.be.captureSelector('#content', function (page) {
-            page.sendKeys("#urlToCheck", 'http://twitter.com/tweet');
-            page.click("#checkurl", 1000);
+            page.sendKeys('[ng-model="urlToCheck"]', 'http://twitter.com/tweet');
+            page.click('[onconfirm="checkResult()"] input', 1000);
         }, done);
     });
 
     it('should disable internal social network list correct', function (done) {
         expect.screenshot('social_disable').to.be.captureSelector('#content', function (page) {
-            page.click("a[role=noDefaultSocials]", 1000);
+            page.click('[ng-click="setUseDefaultSocials(0)"]', 1000);
         }, done);
     });
 
     it('should identify social network correct', function (done) {
         expect.screenshot('social_identify_empty_list').to.be.captureSelector('#content', function (page) {
-            page.sendKeys("#urlToCheck", 'http://twitter.com/tweet');
-            page.click("#checkurl", 1000);
+            page.sendKeys('[ng-model="urlToCheck"]', 'http://twitter.com/tweet');
+            page.click('[onconfirm="checkResult()"] input', 1000);
         }, done);
     });
 
     it('should open add social dialog', function (done) {
-        expect.screenshot('social_add').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click("[role=addSocial]", 500);
+        expect.screenshot('social_add').to.be.captureSelector('#content', function (page) {
+            page.click('[ng-click="showAddSocialForm(true)"]', 500);
         }, done);
     });
 
@@ -93,32 +94,32 @@ describe("ReferrersManager", function () {
         expect.screenshot('social_list_new').to.be.captureSelector('#content', function (page) {
             page.sendKeys("#socialName", 'test social', 150);
             page.sendKeys("#socialHost", 'randomsocial.com', 150);
-            page.click('[role="submitAddSocial"]', 150);
+            page.click('[onconfirm="addSocial()"] input', 150);
         }, done);
     });
 
     it('should identify new social correct', function (done) {
         expect.screenshot('social_identify_new').to.be.captureSelector('#content', function (page) {
-            page.sendKeys("#urlToCheck", 'http://randomsocial.com/123');
-            page.click("#checkurl", 1000);
+            page.sendKeys('[ng-model="urlToCheck"]', 'http://randomsocial.com/123');
+            page.click('[onconfirm="checkResult()"] input', 1000);
         }, done);
     });
 
     it('should enable internal social network list correct', function (done) {
         expect.screenshot('social_enable').to.be.captureSelector('#content', function (page) {
-            page.click("a[role=noDefaultSocials]", 1000);
+            page.click('[ng-click="setUseDefaultSocials(1)"]', 1000);
         }, done);
     });
 
     it('should show remove search engine dialog correct', function (done) {
-        expect.screenshot('social_remove_dialog').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click("[role=removeSocial]", 500);
+        expect.screenshot('social_remove_dialog').to.be.captureSelector('.modal.open', function (page) {
+            page.click('[ng-click="removeSocial(host)"]', 500);
         }, done);
     });
 
     it('should remove new search engine correct', function (done) {
         expect.screenshot('socials_list').to.be.captureSelector('#content', function (page) {
-            page.click(".ui-dialog button>span:contains(Yes)", 500);
+            page.click(".modal.open a>span:contains(Yes)", 500);
         }, done);
     });
 });
