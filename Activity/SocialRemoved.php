7003e978b5c3c54af7afcdafc77440d5b40ca186
sgiehl <stefan@piwik.org>
2016-10-29 16:40:43 +0200

    defines activities for now activitylog plugin

---
+++ Activity/SocialRemoved.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\ReferrersManager\Activity;
+
+use Piwik\Piwik;
+use Piwik\Plugins\ActivityLog\Activity\Activity;
+
+class SocialRemoved extends Activity
+{
+    protected $eventName = 'API.ReferrersManager.removeSocial';
+
+    /**
+     * Returns data to be used for logging the event
+     *
+     * @param array $eventData Array of data passed to postEvent method
+     * @return array
+     */
+    public function extractParams($eventData)
+    {
+        list($finalAPIParameters) = $eventData;
+        list($host) = $finalAPIParameters;
+
+        $userDefinedSocials = \Piwik\Plugins\ReferrersManager\API::getInstance()->getUserDefinedSocials();
+        $name               = $userDefinedSocials[$host];
+
+        return [
+            'items' => [
+                [
+                    'type' => 'searchengine',
+                    'data' => [
+                        'name' => $name,
+                        'host' => $host,
+                    ]
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * Returns the translated description of the logged event
+     *
+     * @param array $activityData
+     * @param string $performingUser
+     * @return string
+     */
+    public function getTranslatedDescription($activityData, $performingUser)
+    {
+        return Piwik::translate('ReferrersManager_SocialRemoved');
+    }
+}
